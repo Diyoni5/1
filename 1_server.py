d684a27330f8dca4d4db72a31a4cb39b459109e4
Ха Ди Ен <kha_diyoni@edu.spbau.ru>
2024-02-20 15:30:04 +0300

    ...

---
+++ 1_server.py
@@ -0,0 +1,26 @@
+import socket
+import logging
+
+logger = logging.getLogger(__name__)
+logging.basicConfig()
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+c = s.bind(('127.0.0.1', 8080))
+s.listen(5)
+s.setblocking(False)
+s.settimeout(5)
+
+while True:
+    try:
+        client_socket = s.accept()
+    except TimeoutError:
+        continue
+    client, (addr, port) = client_socket
+    logger.warning(f"{addr} connected from port {port}")
+
+    incoming = client.recv(500)
+    # print(incoming)
+    content = ''
+    client.send(content)
+    
+    client.close()
