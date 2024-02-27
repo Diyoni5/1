797343a9589941b21c428e093d3cc0156e217bf5
Ха Ди Ен <kha_diyoni@edu.spbau.ru>
2024-02-27 15:46:27 +0300

    life is pain

--- lalalala.py
+++ lalalala.py
@@ -6,4 +6,4 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(AAA)
 data = json.loads(s.recv(500).decode())
 
-print(f"got {data}, {type(data)})
+print(f"got {data}, {type(data)}")
