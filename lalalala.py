bcd3ccb656039f8c450f80cf8fe7456940c4ffea
Ха Ди Ен <kha_diyoni@edu.spbau.ru>
2024-03-05 15:19:49 +0300

    Phoenix

--- lalalala.py
+++
@@ -1,9 +0,0 @@
-import socket
-import json
-from common import AAA
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(AAA)
-data = json.loads(s.recv(500).decode())
-
-print(f"got {data}, {type(data)}")
