9eb70297d301cdba8b61da9c721dd2b113d525a0
Diyoni <diyoni.kha@yandex.ru>
2024-03-18 23:29:32 +0300

    Need some help

---
+++ HW/penguin.py
@@ -0,0 +1,12 @@
+class Penguin:
+    def __init__(self, species, height, age,  *weight):
+        self.species = species
+        self.height = height
+        self.weight = weight
+        self.age = age
+
+    def swim(self):
+        print(f"Penguin's species - {self.species}. "
+                f"Their average height is {self.height} and weight is from {self.weight[0]} to {self.weight[1]} kg. "
+                f"Besides, they can reach the age of {self.age}.")
+
